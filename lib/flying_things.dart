ace34f5ff66328dba755f9403d6eada48b5db973
mejdi14 <mejdiclubiste@gmail.com>
2021-02-19 14:49:49 +0100

    using mixin class

--- lib/flying_things.dart
+++ lib/flying_things.dart
@@ -1,27 +1,39 @@
-abstract class Insect{
-  void crawl(){
+mixin Fluttering{
+  void flutter() {
+    print('fluttering');
+  }
+}
+
+abstract class Insect {
+  void crawl() {
     print('crawling');
   }
 }
 
-abstract class FlyingInsect extends Insect{
-  void flutter(){
-    print('fluttering');
-  }
+abstract class FlyingInsect extends Insect with Fluttering {
 
-  void buzz(){
+
+  void buzz() {
     print('buzzing');
   }
 }
 
-class Mosquito extends FlyingInsect{
-
-
-
-  void doMosquitoThings(){
+class Mosquito extends FlyingInsect {
+  void doMosquitoThings() {
     crawl();
     flutter();
     buzz();
     print('sucking blood');
   }
+}
+
+abstract class Bird with Fluttering{
+  void chirp(){
+    print('chirp chirp');
+  }
+}
+
+class Swallow extends Bird{
+  chirp();
+  flutter();
 }
